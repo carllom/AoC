b6433c13dd6d221c84f3d775f32f445c7211135a
Carl Lom <carl@casalom.se>
2021-12-05 07:52:41 +0100

    Day 5 cleanup

--- Day5.cs
+++ Day5.cs
@@ -1,28 +1,24 @@
-﻿using System.Linq;
-namespace aoc2k21
+﻿namespace aoc2k21
 {
     internal class Day5 : IAocTask
     {
         public long Task1(string indatafile)
         {
-            var indata = File.ReadAllLines(indatafile).Select(Line.ParseLine).ToArray();
+            var lines = File.ReadAllLines(indatafile).Select(Line.ParseLine).ToArray();
+            var xsize = lines.Max(l => Math.Max(l.x1,l.x0));
+            var ysize = lines.Max(l => Math.Max(l.y1,l.y0));
+            var map = new int[xsize+1, ysize+1]; // inclusive max coordinate
 
-            //if (indata.Any(l => l.y1 < l.y0) || indata.Any(l => l.x1 < l.x0)) throw new Exception("input error");
-
-            var maxx = indata.Max(l => Math.Max(l.x1,l.x0));
-            var maxy = indata.Max(l => Math.Max(l.y1,l.y0));
-            var img = new int[maxx+1, maxy+1]; // inclusive max coordinate
-
-            foreach (var l in indata)
+            foreach (var l in lines)
             {
-                // hor
+                // 1. Naive solution using separate code for the different cases
                 if (l.y0 == l.y1)
                 {
                     var xf = Math.Min(l.x0, l.x1);
                     var xt = Math.Max(l.x0, l.x1);
                     for (int x = xf; x <= xt; x++)
                     {
-                        img[x, l.y0]++;
+                        map[x, l.y0]++;
                     }
                 }
                 else if (l.x0 == l.x1)
@@ -31,79 +27,48 @@ namespace aoc2k21
                     var yt = Math.Max(l.y0, l.y1);
                     for (int y = yf; y <= yt; y++)
                     {
-                        img[l.x0, y]++;
+                        map[l.x0, y]++;
                     }
                 }
                 // skip slanted
             }
 
-            var result = 0;
-            for (int x = 0; x <= maxx; x++)
-            {
-                for (int y = 0; y <= maxy; y++)
-                {
-                    if (img[x, y] > 1) result++;
-                }
-
-            }
-
-            return result;
+            return Crossings(map);
         }
 
         public long Task2(string indatafile)
         {
-            var indata = File.ReadAllLines(indatafile).Select(Line.ParseLine).ToArray();
+            var lines = File.ReadAllLines(indatafile).Select(Line.ParseLine).ToArray();
+            var xsize = lines.Max(l => Math.Max(l.x1, l.x0));
+            var ysize = lines.Max(l => Math.Max(l.y1, l.y0));
+            var map = new int[xsize+1, ysize+1]; // inclusive max coordinate
 
-            //if (indata.Any(l => l.y1 < l.y0) || indata.Any(l => l.x1 < l.x0)) throw new Exception("input error");
-
-            var maxx = indata.Max(l => Math.Max(l.x1, l.x0));
-            var maxy = indata.Max(l => Math.Max(l.y1, l.y0));
-            var img = new int[maxx+1, maxy+1]; // inclusive max coordinate
-
-            foreach (var l in indata)
+            foreach (var l in lines)
             {
-                // horizontal
-                if (l.y0 == l.y1)
+                // 2. Nicer generic solution using step iteration instead of x/y iteration
+                var xs = l.x1 == l.x0 ? 0 : l.x1 > l.x0 ? 1 : -1; // x step direction. -1,0 or 1
+                var ys = l.y1 == l.y0 ? 0 : l.y1 > l.y0 ? 1 : -1; // y step direction. -1,0 or 1
+                var steps = Math.Max(Math.Abs(l.x1-l.x0), Math.Abs(l.y1-l.y0)); // Number of steps. abs(xd)=abs(yd) if slanted
+                
+                for (int s = 0; s <= steps; s++)
                 {
-                    var xf = Math.Min(l.x0, l.x1);
-                    var xt = Math.Max(l.x0, l.x1);
-                    for (int x = xf; x <= xt; x++)
-                    {
-                        img[x, l.y0]++;
-                    }
-                }
-                else if (l.x0 == l.x1)
-                {
-                    var yf = Math.Min(l.y0, l.y1);
-                    var yt = Math.Max(l.y0, l.y1);
-                    for (int y = yf; y <= yt; y++)
-                    {
-                        img[l.x0, y]++;
-                    }
-                }
-                else
-                {
-                    var xs = l.x1 > l.x0 ? 1 : -1; // x step direction
-                    var ys = l.y1 > l.y0 ? 1 : -1; // y step direction
-
-                    // Step
-                    for (int s = 0; s <= Math.Abs(l.x1-l.x0); s++) // xd=yd since diagonal is 45 degrees
-                    {
-                        img[l.x0+(s*xs), l.y0+(s*ys)]++;
-                    }
+                    map[l.x0+(s*xs), l.y0+(s*ys)]++;
                 }
             }
 
+            return Crossings(map);
+        }
+
+        private int Crossings(int[,] map)
+        {
             var result = 0;
-            for (int x = 0; x <= maxx; x++)
+            for (int x = 0; x <= map.GetUpperBound(0); x++)
             {
-                for (int y = 0; y <= maxy; y++)
+                for (int y = 0; y <= map.GetUpperBound(1); y++)
                 {
-                    if (img[x, y] > 1) result++;
+                    if (map[x, y] > 1) result++;
                 }
-
             }
-
             return result;
         }
     }
