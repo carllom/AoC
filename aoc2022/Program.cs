757982f635e544d51a740e66cb811fbfed3961fa
Carl Lom <carl@casalom.se>
2022-12-01 06:33:52 +0100

    Day 1 - Calorie Counting

---
+++ aoc2022/Program.cs
@@ -0,0 +1,29 @@
+﻿using common;
+using System.Reflection;
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        Console.WriteLine("=========================");
+        Console.WriteLine("== Advent of Code 2022 ==");
+        Console.WriteLine("=========================");
+
+        var engine = new AocTaskEnumerator(Assembly.GetExecutingAssembly());
+
+        if (args.Length > 0)
+        {
+            if (!int.TryParse(args[0], out int day))
+            {
+                Console.Error.WriteLine($"Expected single integer (day) argument");
+                return -1;
+            }
+            engine.Execute(day);
+        }
+        else
+        {
+            engine.Execute();
+        }
+        return 0;
+    }
+}
---
+++ common/AocInputHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace common
+{
+    public static class AocInput
+    {
+        private static string InputDir => Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\", "input");
+        private static string InputPath(int day, int task) => Path.Combine(InputDir, $"day{day}-{task}.input");
+        private static string InputPath(int day) => Path.Combine(InputDir, $"day{day}.input");
+
+        public static string[] GetLines(int day, int task)  => File.ReadAllLines(InputPath(day, task));
+        public static string[] GetLines(int day) => File.ReadAllLines(InputPath(day));
+        public static string GetText(int day, int task) => File.ReadAllText(InputPath(day, task));
+        public static string GetText(int day) => File.ReadAllText(InputPath(day));
+    }
+}
