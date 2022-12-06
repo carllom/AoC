e91aee6b1da3908192b83f17718db807ee48ae7d
Carl Lom <carl@casalom.se>
2022-12-06 07:10:25 +0100

    Day 6 - Tuning Trouble

---
+++ aoc2022/Day6.cs
@@ -0,0 +1,23 @@
+﻿using common;
+
+namespace aoc2022
+{
+    [AocDay(6, Caption = "Tuning Trouble")]
+    internal class Day6
+    {
+        [AocTask(1)]
+        public int Task1() => FindDistinct(AocInput.GetText(6), 4);
+
+        [AocTask(2)]
+        public int Task2() => FindDistinct(AocInput.GetText(6), 14);
+
+        private int FindDistinct(string data, int size)
+        {
+            for (int i = 0; i < data.Length - size; i++)
+            {
+                if (data.Skip(i).Take(size).Distinct().Count() == size) return i + size; // Wasteful but elegant
+            }
+            return int.MinValue;
+        }
+    }
+}
