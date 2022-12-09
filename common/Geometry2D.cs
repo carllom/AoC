13d6a412a414f02909d7d55ac3e30349ac9770b0
Carl Lom <carl@casalom.se>
2022-12-09 07:38:46 +0100

    Day 9 - Rope Bridge

---
+++ common/Geometry2D.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace common
+{
+    public struct Point2
+    {
+        public int x, y;
+        public Point2(int x, int y) { this.x = x; this.y = y; }
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if (!(obj is Point2)) return false;
+            var o = (Point2)obj;
+            return x==o.x && y == o.y;
+        }
+        public override int GetHashCode() => HashCode.Combine(x, y);
+        public static Point2 operator -(Point2 a, Point2 b) => new(a.x-b.x, a.y-b.y);
+        public override string ToString() => $"[{x},{y}]";
+    }
+}
