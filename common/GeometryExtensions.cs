4295ac557d7c4f88d4198cf75327d7ccc8a2ca58
Carl Lom <carl@casalom.se>
2022-12-14 20:05:29 +0100

    Day 14 - Regolith Reservoir

---
+++ common/GeometryExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace common
+{
+    public static class GeometryExtensions
+    {
+        public static Rectangle Span(this IEnumerable<Point2> points)
+        {
+            Rectangle span = new(int.MaxValue, int.MaxValue, int.MinValue, int.MinValue);
+            foreach (var p in points) 
+            { 
+                span.x0 = int.Min(span.x0, p.x);
+                span.x1 = int.Max(span.x1, p.x);
+                span.y0 = int.Min(span.y0, p.y);
+                span.y1 = int.Max(span.y1, p.y);
+            }
+            return span;
+        }
+    }
+}
