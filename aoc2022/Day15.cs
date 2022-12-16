345eda78caf36598d49174f4b15ea5f00f25a2d7
Carl Lom <carl@casalom.se>
2022-12-16 03:24:56 +0100

    Day 15 - Beacon Exclusion Zone - brute force

---
+++ aoc2022/Day15.cs
@@ -0,0 +1,120 @@
+﻿using common;
+
+namespace aoc2022
+{
+    [AocDay(15, Caption = "")]
+    internal class Day15
+    {
+        [AocTask(1)]
+        public int Task1()
+        {
+            var input = AocInput.GetLines(15, false).Select(l => l.Split(':').Select(c => c.Split("at ")[1])
+                .Select(c => { var x = c.Split(',', '='); return new Point2(int.Parse(x[1]), int.Parse(x[3])); }).ToArray()).Select(c => new SbPair(c[0], c[1]));
+
+            Rectangle span = new(int.MaxValue, int.MaxValue, int.MinValue, int.MinValue);
+            foreach (var p in input)
+            {
+                span.x0 = int.Min(span.x0, p.sensor.x - p.dist);
+                span.x1 = int.Max(span.x1, p.sensor.x + p.dist);
+                span.y0 = int.Min(span.y0, p.sensor.y - p.dist);
+                span.y1 = int.Max(span.y1, p.sensor.y + p.dist);
+            }
+
+            var y = 2000000;
+            var ranges = new List<Range1D>();
+            foreach (var sb in input)
+            {
+                if (!sb.Covers(y)) continue;
+                ranges.Add(sb.SpanAt(y));
+            }
+            ranges.Sort((a, b) => a.from - b.from);
+            var nurng = new List<Range1D>();
+            var g = ranges[0];
+            for (int i = 1; i < ranges.Count; i++)
+            {
+                var q = g.Add(ranges[i]);
+                if (q.Length > 1) nurng.Add(q[0]);
+                g = q[q.Length-1];
+            }
+            nurng.Add(g);
+            return nurng.Sum(r => r.Size);
+        }
+
+        [AocTask(2)]
+        public long Task2()
+        {
+            var input = AocInput.GetLines(15, false).Select(l => l.Split(':').Select(c => c.Split("at ")[1])
+                .Select(c => { var x = c.Split(',', '='); return new Point2(int.Parse(x[1]), int.Parse(x[3])); }).ToArray()).Select(c => new SbPair(c[0], c[1]));
+
+            Rectangle span = new(int.MaxValue, int.MaxValue, int.MinValue, int.MinValue);
+            foreach (var p in input)
+            {
+                span.x0 = int.Min(span.x0, p.sensor.x - p.dist);
+                span.x1 = int.Max(span.x1, p.sensor.x + p.dist);
+                span.y0 = int.Min(span.y0, p.sensor.y - p.dist);
+                span.y1 = int.Max(span.y1, p.sensor.y + p.dist);
+            }
+
+            var limit = 4000000;
+            for (int y = 0; y <= limit; y++)
+            {
+                var ranges = new List<Range1D>();
+                foreach (var sb in input)
+                {
+                    if (!sb.Covers(y)) continue;
+                    var sp = sb.SpanAt(y);
+                    if (sp.to < 0 || sp.from > limit) continue;
+                    ranges.Add(sp);
+                }
+
+                ranges.Sort((a, b) => a.from - b.from);
+                var nurng = new List<Range1D>();
+                var g = ranges[0];
+                for (int i = 1; i < ranges.Count; i++)
+                {
+                    var q = g.Add(ranges[i]);
+                    if (q.Length > 1) nurng.Add(q[0]);
+                    g = q[q.Length-1];
+                }
+                nurng.Add(g);
+                if (nurng.Count > 1)
+                {
+                    var h = new Range1D(0, limit);
+                    var i = h.Subtract(nurng[0])[0].Subtract(nurng[1])[0];
+                    return 4000000L*i.from + y;
+                }
+                if (y%100000==0) Console.Write('.');
+                //return nurng.Sum(r => r.Size);
+            }
+
+            return default;
+        }
+    }
+
+    public class SbPair
+    {
+        public Point2 sensor;
+        public Point2 beacon;
+        public int dist;
+        public int firstY;
+        public int lastY;
+
+        public SbPair(Point2 sensor, Point2 beacon) { 
+            this.sensor = sensor;
+            this.beacon = beacon;
+            dist = sensor.ManhDist(beacon);
+            firstY = sensor.y - dist;
+            lastY = sensor.y + dist;
+        }
+
+        public bool Covers(int y) => firstY <= y && lastY >= y;
+
+        public Range1D SpanAt(int y)
+        {
+            var off = Math.Abs(y-sensor.y);
+            if (off > dist) throw new ApplicationException(); // too far away
+            var delt = dist - off;
+            return new Range1D(sensor.x - delt,sensor.x + delt);
+        }
+    }
+}
--- common/Geometry2D.cs
+++ common/Geometry2D.cs
@@ -15,6 +15,7 @@ namespace common
         public override int GetHashCode() => HashCode.Combine(x, y);
         public static Point2 operator -(Point2 a, Point2 b) => new(a.x-b.x, a.y-b.y);
         public override string ToString() => $"[{x},{y}]";
+        public int ManhDist(Point2 other) => Math.Abs(x-other.x) + Math.Abs(y-other.y);
     }
 
     public struct Rectangle
@@ -28,5 +29,43 @@ namespace common
         public Rectangle(int x0, int y0, int x1, int y1) { this.x0 = x0; this.x1 = x1; this.y0 = y0; this.y1 = y1; }
         public Rectangle(Point2 p0, int width, int height) { x0 = p0.x; y0 = p0.y; x1 = x0+width; y1 = y0+height; }
         public Rectangle(Point2 p0, Point2 p1) { x0 = p0.x; y0 = p0.y; x1 = p1.x; y1 = p1.y; }
+    }
+
+    public struct Range1D
+    {
+        public int from;
+        public int to;
+        public Range1D(int from, int to) { this.from = from; this.to = to; }
+
+
+        public bool Contains(Range1D other) => from <= other.from && to >= other.to;
+        public bool Overlaps(Range1D other) => other.from <= to && other.to >= from;
+
+        public Range1D[] Subtract(Range1D other)
+        {
+            if (!Overlaps(other)) return new[] { this };
+            if (other.Contains(this)) return new Range1D[0];
+
+            if (other.from > from && other.to < to) return new Range1D[] { new(from, other.from-1), new(other.to+1, to) }; // Cut in the middle
+
+            if (other.from < from) // Cut left side
+            {
+                return new[] { new Range1D(Math.Max(from, other.to+1), to) };
+            }
+            else // cut right side
+            {
+                return new[] { new Range1D(from, Math.Min(to, other.from-1)) };
+            }
+
+        }
+
+        public Range1D[] Add(Range1D other)
+        {
+            //if (Contains(other)) return new[] { this };
+            if (!Overlaps(other)) return new [] { this, other };
+            return new[] { new Range1D(Math.Min(from, other.from), Math.Max(to, other.to)) };
+        }
+
+        public int Size => to - from;
     }
 }
